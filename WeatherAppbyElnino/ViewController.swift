b7a6cc5429c4f0ab7ed7e87fe792175904d75573
Erick Tshimbombo <erick.tshimbombo@yahoo.com>
2020-06-19 15:03:32 +0200

    weatherappupdate

--- WeatherAppbyElnino/ViewController.swift
+++ WeatherAppbyElnino/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController,CLLocationManagerDelegate {
+
+    
+    @IBOutlet weak var temperaturLabel: UILabel!
+    
+    @IBOutlet weak var conditionIamge: UIImageView!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    let LocationManager:CLLocationManager = CLLocationManager()
+   
+    
+    var weatherURL :String = "http://api.openweathermap.org/data/2.5/weather"
+    var AppID:String = "6b5f4ede3ccb0fc1a67feecad0e23773"
+    
+  
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        LocationManager.delegate = self
+        LocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        LocationManager.requestWhenInUseAuthorization()
+        LocationManager.startUpdatingLocation()
     }
-
+    
+  
+    func getLocationWeatherData(url:String,parameters:[String:String])
+    {
+        
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        
+        if location.horizontalAccuracy > 0
+        {
+            let locationdata = location.coordinate
+            print(locationdata)
+            
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        cityLabel.text = "Location not found"
+    }
+  
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        
+    }
+   
+    
+    @IBAction func forward(_ sender: Any)
+    {
+        performSegue(withIdentifier: "passData", sender: self)
+    }
+    
+    
 
 }
 
